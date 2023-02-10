04f127102f06dd976d73be35b4f4de4cc83085cd
thanakorn-student <6472031921@student.chula.ac.th>
2023-02-11 00:48:29 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
 
 //Route files
 const hospitals = require ('./routes/hospitals');
 
-//Load env vars
-dotenv.config({path:'./config/config.env'});
-
 const app=express();
 
+//Body parser
+app.use(express.json());
+
 //Mount routers
-app.use('/app/v1/hospitals',hospitals);
+app.use('/api/v1/hospitals',hospitals);
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5001;
 
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
+const server = app.listen(PORT, () => {console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error ${err.message}`);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+  });
